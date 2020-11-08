3d5c1041be0034aec81536476c823ac48afad9ae
mizu11pro <mizuakasa@gmail.com>
2020-11-08 09:40:35 +0000

    [finish] 8-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'booklists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'books#top'
+  post 'booklists' => 'booklists#create'
 end
