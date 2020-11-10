3b077a232d2b6acd221faa6f0d5d592149bd82b8
mizu11pro <mizuakasa@gmail.com>
2020-11-10 13:19:09 +0000

    [finish] 8-9

--- app/controllers/booklists_controller.rb
+++ app/controllers/booklists_controller.rb
@@ -1,7 +1,4 @@
 class BooklistsController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     book = Book.new(book_params)
@@ -11,6 +8,7 @@ class BooklistsController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
