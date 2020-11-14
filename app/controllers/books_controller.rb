ad49eb24e8e88cfff8686f1dc19a9877e1dc538b
mizu11pro <mizuakasa@gmail.com>
2020-11-14 07:14:16 +0000

    [終了]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,58 @@
 class BooksController < ApplicationController
-  def top
+   def top
+   end
+
+   def new
+   @book = Book.new
+   end
+
+   def create
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book)
+    else
+      @books = Book.all
+    render "index"
+    end
+   end
+
+   def index
+    @books = Book.all
+    @book = Book.new
+   end
+
+   def show
+    # /books/:id
+    # /books/1000
+    @book = Book.find(params[:id])
+   end
+
+   def edit
+    @book = Book.find(params[:id])
+    flash[:notice] = "Book was successfully updated."
+   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end
   end
+
+  def destroy
+   book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+    redirect_to book_path
+    end
+  end
+
+  private
+  def book_params
+    params. require(:book).permit(:title, :body,)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'books' => 'booklists#create'
-  get 'books' => 'booklists#index'
-  get 'books/:id' => 'booklists#show', as: 'booklist'
-  get 'books/:id/edit' => 'booklists#edit', as: 'edit_booklist'
-  patch 'books/:id' => 'booklists#update', as: 'update_booklist'
-  delete 'books/:id' => 'booklists#destroy', as: 'destroy_booklist'
+  resources :books
   root to: 'books#top'
 end
